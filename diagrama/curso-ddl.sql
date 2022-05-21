ad5cc862f37b67fb56b2288a53e80671cebd0827
Dagmar87 <jdfssobrinho@hotmail.com>
2022-05-21 12:38:51 -0300

    atualização do arquivo ddl

--- diagrama/curso-ddl.sql
+++ diagrama/curso-ddl.sql
@@ -9,3 +9,22 @@ CREATE TABLE IF NOT EXISTS "cursos" (
 "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
 PRIMARY KEY ("id")
 );
+
+--CREATE
+INSERT INTO "cursos" ("id","nome","professor","sala","horarioInicio","horarioFim","createdAt","updatedAt") VALUES (DEFAULT,$1,$2,$3,$4,$5,$6,$7) RETURNING "id","nome","professor","sala","horarioInicio","horarioFim","createdAt","updatedAt";
+
+--FINDALL
+SELECT "id", "nome", "professor", "sala", "horarioInicio", "horarioFim", "createdAt", "updatedAt" FROM "cursos" AS "curso";
+
+--FINDONE
+SELECT "id", "nome", "professor", "sala", "horarioInicio", "horarioFim", "createdAt", "updatedAt" FROM "cursos" AS "curso" WHERE "curso"."id" = '2';
+
+--UPDATE
+UPDATE "cursos" SET "professor"=$1,"updatedAt"=$2 WHERE "id" = $3
+
+--DELETE
+DELETE FROM "cursos" WHERE "id" = '4'
+
+--DELETEALL
+DELETE FROM "cursos"
+
