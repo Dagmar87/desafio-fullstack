6eadcbdb84ea544adf847e5c8fe53eef4dd53a41
Dagmar87 <jdfssobrinho@hotmail.com>
2022-05-21 10:47:37 -0300

    criação dos arquivos .js da pasta app do projeto

---
+++ curso-nodejs-postgresql/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const db = require("./app/models");
+db.sequelize.sync({ force: true}).then(() => {
+    console.log("Solte e sincronize novamente o db.");
+});
+app.get('/', (req, res) => {
+    res.json({ message: "Bem-vindo aplicação de Gerência de Cursos."});
+});
+require("./app/routes/curso.routes.js")(app);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`O servidor está rodando na porta ${PORT}.`);
+});
