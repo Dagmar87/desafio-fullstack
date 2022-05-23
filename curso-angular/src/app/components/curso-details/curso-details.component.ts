ec82a1c0d9b57b1662a5931d9daaa91df506aedc
Dagmar87 <jdfssobrinho@hotmail.com>
2022-05-23 15:58:57 -0300

    edição do componente curso-details

--- curso-angular/src/app/components/curso-details/curso-details.component.ts
+++ curso-angular/src/app/components/curso-details/curso-details.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Curso } from 'src/app/models/curso.model';
+import { CursoService } from 'src/app/services/curso.service';
 
 @Component({
   selector: 'app-curso-details',
   templateUrl: './curso-details.component.html',
-  styleUrls: ['./curso-details.component.css']
+  styleUrls: ['./curso-details.component.css'],
 })
 export class CursoDetailsComponent implements OnInit {
-
   @Input() viewMode = false;
 
   @Input() currentCurso: Curso = {
@@ -15,12 +16,54 @@ export class CursoDetailsComponent implements OnInit {
     professor: '',
     sala: 0,
     horarioInicio: new Date(),
-    horarioFim: new Date()
-  }
+    horarioFim: new Date(),
+  };
 
-  constructor() { }
+  message = '';
+
+  constructor(
+    private cursoService: CursoService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    if (!this.viewMode){
+      this.message = '';
+      this.getCurso(this.route.snapshot.params["id"]);
+    }
   }
 
+  getCurso(id: string): void {
+    this.cursoService.get(id).subscribe({
+      next: (data) => {
+        this.currentCurso = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e),
+    });
+  }
+
+  updateCurso(): void {
+    this.message = '';
+    this.cursoService.update(this.currentCurso.id, this.currentCurso)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.message = res.message ? res.message : 'Este curso foi atualizado com sucesso!';
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  deleteCurso(): void {
+    this.cursoService.delete(this.currentCurso.id)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate(['/cursos']);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 }
